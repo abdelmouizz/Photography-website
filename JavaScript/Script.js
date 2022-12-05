4418422df020a0fcc2633a4db01cd78af2699b38
Abdelmouiz <abdelmouiz_@outlook.fr>
2022-12-05 21:45:06 +0100

    adding web

---
+++ JavaScript/Script.js
@@ -0,0 +1,16 @@
+
+/* SlideShow */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("myslider");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
